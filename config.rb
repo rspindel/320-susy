4d1f336a6d2e44f0951d1788d1d8bc30fa7b240c
Eric Meyer <eric@oddbird.net>
2011-04-26 16:36:28 -0600

    susy320 initial setup for git

---
+++ config.rb
@@ -0,0 +1,16 @@
+# Compass CSS framework config file
+
+require 'susy'
+
+project_type = :stand_alone
+# Set this to the root of your project when deployed:
+http_path = "/"
+sass_dir = "sass"
+css_dir = "css"
+images_dir = "images"
+fonts_dir = "fonts"
+javascripts_dir = "js"
+line_comments = false
+preferred_syntax = :sass
+output_style = :compact
+relative_assets = true
